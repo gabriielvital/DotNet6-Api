67f690b1d8ac96bfce14ca334ffcc8bc3ed9ff84
Gabriel Vital <gabrit_8@hotmail.com>
2024-01-19 09:25:52 -0300

    Criando a injeção de dependência e ajustando a Main do API

--- GB.ApiDotNet6/GB.ApiDotNet6.Infra.IoC/DependencyInjection.cs
+++ GB.ApiDotNet6/GB.ApiDotNet6.Infra.IoC/DependencyInjection.cs
@@ -1,20 +1,31 @@
-﻿using GB.ApiDotNet6.Infra.Data.Context;
+﻿using GB.ApiDotNet6.Application.Mappings;
+using GB.ApiDotNet6.Application.Services;
+using GB.ApiDotNet6.Application.Services.Interfaces;
+using GB.ApiDotNet6.Domain.Repositories;
+using GB.ApiDotNet6.Infra.Data.Context;
+using GB.ApiDotNet6.Infra.Data.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace GB.ApiDotNet6.Infra.IoC
 {
     public static class DependencyInjection
     {
-        public static IServiceCollection AddInfraestructure(this IServiceCollection services, IConfiguration configuration) 
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration) 
         {
-            services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(configuration.GetConnectionString);
+            services.AddDbContext<ApplicationDbContext>(options => 
+                                    options.UseNpgsql(configuration.GetConnectionString("")));
+            services.AddScoped<IPersonRepository, PersonRepository>();
+            return services;
+        }
+
+        public static IServiceCollection AddServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddAutoMapper(typeof (DomainToDtoMapping));
+            services.AddScoped<IPersonService, PersonService>();
+            return services;
         }
     }
 }

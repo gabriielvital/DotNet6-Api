bef03c709dade04024a8db9cce5caafe6402be08
Gabriel Vital <gabrit_8@hotmail.com>
2024-01-20 07:09:20 -0300

    Criei os métodos get de pessoa
    criei tanto o getbyid quanto o get(getall) de pessoa.

--- GB.ApiDotNet6/GB.ApiDotNet6.Api/Controllers/PersonController.cs
+++ GB.ApiDotNet6/GB.ApiDotNet6.Api/Controllers/PersonController.cs
@@ -7,7 +7,7 @@ namespace GB.ApiDotNet6.Api.Controllers
     
     [ApiController]
     [Route("api/[controller]")]
-    public class PersonController : Controller
+    public class PersonController : ControllerBase
     {
         private readonly IPersonService _personService;
 
@@ -21,6 +21,26 @@ namespace GB.ApiDotNet6.Api.Controllers
         {
             var result = await _personService.CreateAsync(personDTO);
             if(result.IsSucess)
+                return Ok(result);
+            return BadRequest(result);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Get()
+        {
+            var result = await _personService.GetAsync();
+            if(result.IsSucess)
+                return Ok(result);
+            return BadRequest(result);
+        }
+
+
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<ActionResult> GetByIdAsync(int id)
+        {
+            var result = await _personService.GetByIdAsync(id);
+            if (result.IsSucess)
                 return Ok(result);
             return BadRequest(result);
         }
--- GB.ApiDotNet6/GB.ApiDotNet6.Application/Services/PersonService.cs
+++ GB.ApiDotNet6/GB.ApiDotNet6.Application/Services/PersonService.cs
@@ -32,5 +32,17 @@ namespace GB.ApiDotNet6.Application.Services
             return ResultService.Ok<PersonDTO>(_mapper.Map<PersonDTO>(data));
 
         }
+
+        public async Task<ResultService<ICollection<PersonDTO>>> GetAsync()
+        {
+            var people = await _personRepository.GetPeopleAsync();
+            return ResultService.Ok<ICollection<PersonDTO>>(_mapper.Map<ICollection<PersonDTO>>(people));
+        }
+
+        public async Task<ResultService<PersonDTO>> GetByIdAsync(int id)
+        {
+            var person = await _personRepository.GetByIdAsync(id);
+            return ResultService.Ok(_mapper.Map<PersonDTO>(person));
+        }
     }
 }
